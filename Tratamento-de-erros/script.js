927c485ff16c10c7a4d6ba30068b8f7e3a467cd5
IACarvalho <israelcarvalhodesenvolvedor@gmail.com>
2022-03-04 23:28:34 -0300

    Error handling

---
+++ Tratamento-de-erros/script.js
@@ -0,0 +1,25 @@
+class CustomError extends Error {
+  constructor({ message, data }) {
+    super(message)
+    this.data = data
+  }
+}
+
+try {
+  const name = 'Israel Afonso';
+  
+  const myError = new CustomError({
+    message: 'Custom message on custom error',
+    data: {
+      type: 'Server error'
+    }
+  })
+
+  throw myError
+} catch(err) {
+  console.log(err)
+  console.log(err.data)
+}
+finally{
+  console.log('Keep going...');
+}
